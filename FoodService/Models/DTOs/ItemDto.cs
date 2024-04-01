7053b6508d8fd6b898024d4e6165881965dacd08
accesable <trannhutanh654@gmail.com>
2024-04-01 09:30:22 +0700

    Remove FoodService

--- FoodService/Models/DTOs/ItemDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductService.Models.DTOs
-{
-    public class ItemDto
-    {
-        public int ItemId { get; set; }
-        public string? ItemName { get; set; }
-
-    }
-}
--- FoodService/Models/RequestModels/CategoryRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebApplication1.Models.RequestModels
-{
-    public class CategoryRequest
-    {
-        public string ? CategoryName { get; set; }
-    }
-}
